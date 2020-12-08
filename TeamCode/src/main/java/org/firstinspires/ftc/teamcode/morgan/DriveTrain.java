648c63bbcb2e77aa2d7c490e1ecb18a21254f90d
DESKTOP-HK4E3PG\markh <markhazen05@hotmail.com>
2020-12-07 20:43:28 -0600

    no message

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/morgan/DriveTrain.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.morgan;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class DriveTrain {
+
+    DcMotor leftFront, rightFront, leftBack, rightBack;
+
+    public DriveTrain () {
+
+    }
+
+    public void DriveTrainComtr
+
+}

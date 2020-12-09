8cb9f7a65c7d5bc222580bd311912452b7c72056
DESKTOP-LLS2934\Revolutionary Robots <revrobots@gmail.com>
2020-12-09 17:45:50 -0600

    Added the intake wheels to the controls of the intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/morgan/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/morgan/Intake.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.morgan;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
@@ -9,19 +10,49 @@ public class Intake {
 
     DcMotor intake;
 
-    public Intake (Gamepad g1, DcMotor i) {
+    CRServo intakeWheels;
+
+    int intakeControl = 1;
+
+    public Intake (Gamepad g1, DcMotor i, CRServo iW) {
         gamepad1 = g1;
 
         intake = i;
         i.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        intakeWheels = iW;
     }
 
     public void intakeControl () {
 
-        if (gamepad1.right_trigger != 0) {
-            intake.setPower(gamepad1.right_trigger);
-        } else if (gamepad1.left_trigger != 0) {
-            intake.setPower(-gamepad1.left_trigger);
+        if (gamepad1.dpad_up) {
+            intakeControl = 1;
+        } else if (gamepad1.dpad_left) {
+            intakeControl = 2;
+        } else if (gamepad1.dpad_right) {
+            intakeControl = 3;
+        }
+
+        if (intakeControl == 1) {
+            if (gamepad1.right_trigger != 0) {
+                intake.setPower(gamepad1.right_trigger);
+                intakeWheels.setPower(gamepad1.right_trigger);
+            } else if (gamepad1.left_trigger != 0) {
+                intake.setPower(-gamepad1.left_trigger);
+                intakeWheels.setPower(-gamepad1.left_trigger);
+            }
+        } else if (intakeControl == 2) {
+            if (gamepad1.right_trigger != 0) {
+                intake.setPower(gamepad1.right_trigger);
+            } else if (gamepad1.left_trigger != 0) {
+                intake.setPower(-gamepad1.left_trigger);
+            }
+        } else if (intakeControl == 3) {
+            if (gamepad1.right_trigger != 0) {
+                intakeWheels.setPower(gamepad1.right_trigger);
+            } else if (gamepad1.left_trigger != 0) {
+                intakeWheels.setPower(-gamepad1.left_trigger);
+            }
         }
 
     }
